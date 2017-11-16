5b71cebee8992b2597ef0369448a2bec3aeaac14
Arthur Poiret <dropsnorz@gmail.com>
2017-11-16 11:45:53 +0000

    Fix ore select request

--- src/refinery/repositories/OreRepository.php
+++ src/refinery/repositories/OreRepository.php
@@ -6,7 +6,7 @@ class OreRepository extends EntityRepository{
 
 	public function findAllPrimaryOreSecondaryRelated()
 	{
-	   $dql = "SELECT p, s, pom, m FROM PrimaryOre p JOIN p.secondaryOres s LEFT JOIN p.reprocessedMinerals pom LEFT JOIN pom.mineral m ORDER By p.normalizedPrice DESC";
+	   $dql = "SELECT p, s, pom, m FROM PrimaryOre p LEFT JOIN p.secondaryOres s LEFT JOIN p.reprocessedMinerals pom LEFT JOIN pom.mineral m ORDER By p.normalizedPrice DESC";
        $query = $this->getEntityManager()->createQuery($dql);
 
        return $query->getArrayResult();
